d4b08f42b69bf40e4fac38228480431b74d74212
Kirill Sukharev <aruter@yandex.ru>
2020-10-10 20:58:33 +0300

    homework lesson 02 hard is done

--- lesson02/scripts/main.js
+++ lesson02/scripts/main.js
@@ -5,14 +5,31 @@ let money = 1000000,
     mission = 5000000, 
     period = 9,
     budgetDay = money/30,
-    addExpensesList = addExpenses.toLocaleLowerCase().split('');
+    num = 266219,
+    numArr = num.toString().split(''),
+    numResult = 1;
 
+numList.forEach(function(i){
+    numResult *= i;
+});
+exNumResult = numResult**3;
+numResultArr = exNumResult.toString().split('');
+
+//low block
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей");
-console.log(addExpensesList);
+console.log(addExpenses.toLocaleLowerCase().split(''));
 console.log(budgetDay);
+// Hard block
+console.log(numResult);
+console.log(exNumResult);
+console.log(numResultList[0], numResultList[1]);
+
+
+
+
 
