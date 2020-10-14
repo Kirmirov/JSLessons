a32f3d8ee2a342739994f92838794e895c072737
Kirill Sukharev <aruter@yandex.ru>
2020-10-14 20:41:12 +0300

    lesson05_hard is done

--- lesson05/scripts/main.js
+++ lesson05/scripts/main.js
@@ -1,70 +1,14 @@
-let money, 
-    income = '2000000', 
-    addExpenses,
-    deposit, 
-    mission = 1000000,
-    result,
-    status,
-    realyExp,
-    expenses,
-    fanExp,
-    period = 9,
-    accumulatedMonth = 0,
-    budgetDay = 0;
+let arr = ['488447', '3987', '20398', '29300', '40988', '39876', '98764'];
 
-start(); 
+arr.forEach(function(i){
+    let check = i.charAt(0);
+    if(check == '2' || check == '4') console.log(i);
+});
 
-function isNumber (n){
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-function start(){
-    do money = +prompt('Укажите Ваш ежемесячный доход');
-    while(!isNumber(money)) {
-        addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        deposit = confirm('Есть ли у вас депозит в банке?');
-        expenses = getExpensesMonth();
-        accumulatedMonth = getAccumulatedMonth(money, expenses);
-        result = getTargetMonth(mission, accumulatedMonth);
-        budgetDay = accumulatedMonth/30;
+begin:
+for(i = 2; i <= 100; i++){
+    for (n = 2; n < i; n++){
+        if (i%n == 0) continue begin;
     }
-};
-
-function showTypeOf (data){
-    return typeof data;
-};
-
-function getExpensesMonth (){
-    let sum = 0;
-    for(i = 0; i < 2; i++){
-            if (i==0) realyExp = prompt('Какие обязательные расходы вы планируете в месяце?');
-            else fanExp = prompt('Какие еще расходы вы планируете в месяце?');
-        do amount = +prompt('Во сколько это обойдется?');
-        while(!isNumber(amount)) sum += amount;
-    }
-    return sum;
-};
-
-function getAccumulatedMonth (money, expenses){
-    return money - expenses;
-};
-
-function getTargetMonth (mission, accumulatedMonth){
-    let res = Math.ceil(mission/accumulatedMonth);
-        if(res >= 0) return 'Цель будет достигнута через ' + res + ' месяцев';
-        else return 'Цель не будет достигнута';
-};
-
-function getStatusIncome (income){
-    if(income) return true;
-    else return false;
-};
-
-console.log(showTypeOf(money));
-console.log(showTypeOf(income));
-console.log(showTypeOf(deposit));
-console.log('Расходы за месяц составили ' + expenses + ' рублей');
-console.log('Бюджет в месяц составляет ' + accumulatedMonth + ' рублей');
-console.log(result);
-console.log('Дневной бюджет составляет ' + budgetDay + ' рублей');
-console.log(getStatusIncome(income));
+    console.log(i);
+}
