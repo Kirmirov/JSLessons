32b5c283e8d37f28930d0d51163c466da66856d7
Kirill Sukharev <aruter@yandex.ru>
2020-10-28 01:33:13 +0300

    lesson14_hard is done

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 const body = document.querySelector('body');
 
-function DomElement (selector, height, width, bg, fontSize) {
+function DomElement (selector, height, width, bg, fontSize, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.position = position;
 };
 
 DomElement.prototype.createElem = function (){
@@ -22,13 +23,36 @@ DomElement.prototype.createElem = function (){
     width:  ${this.width}px;
     background:  ${this.bg};
     font-size:  ${this.fontSize}px;
+    position: ${this.position};
     `;
-    element.textContent = '!CREATE ME MASTER!';
+    element.textContent = '!MOVE ME MASTER!';
     body.append(element);
 };
 
-let dom = new DomElement('#rahga', '60', '60', 'red', '30');
-dom.createElem();
-
-let dom2 = new DomElement('.lock', '80', '100', 'green', '20');
-dom2.createElem();
+DomElement.prototype.movement = function (){
+    document.addEventListener('keydown', function (event) {
+        const square = document.querySelector('div');
+        let squareLeft = parseInt(square.style.left);
+        let squareTop = parseInt(square.style.top);
+        if (!isNaN(square.style.left)) squareLeft = 0;
+        if (!isNaN(square.style.top)) squareTop = 0;
+        if (event.code == 'ArrowLeft') {
+            squareLeft += -10;
+            square.style.left = squareLeft + 'px';  
+        } else if (event.code == 'ArrowUp') {
+            squareTop += -10;
+            square.style.top = squareTop + 'px';
+        } else if (event.code == 'ArrowRight') {
+            squareLeft += +10;
+            square.style.left = squareLeft + 'px';
+        } else if (event.code == 'ArrowDown') {
+            squareTop += 10;
+            square.style.top = squareTop + 'px';
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const Dom = new DomElement('.charg', '100', '100', 'red', '20', 'absolute');
+    Dom.createElem();
+    Dom.movement();
+});
