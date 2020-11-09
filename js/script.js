9c5e25532e550980a6e275fd854c38eee039405c
Kirill Sukharev <aruter@yandex.ru>
2020-11-09 20:17:18 +0300

    kfk

--- js/script.js
+++ js/script.js
@@ -7,52 +7,84 @@ class ToDo {
         this.todoList = document.querySelector(todoList);
         this.todoCompleted = document.querySelector(todoCompleted),
         this.todoContainer = document.querySelector(todoContainer),
+        this.isEditTodo = false;
         this.todoData = new Map (JSON.parse(localStorage.getItem('ToDo')));
     };
     init(){
         this.form.addEventListener('submit', this.addTodo.bind(this));
         this.todoContainer.addEventListener('click', this.handler.bind(this));
-        this.render();
-    };
-    handler(e){
-        let target = e.target;
-        if(target.classList.contains('todo-remove')) this.deleteItem(target.closest('.todo-item').key);
-        if(target.classList.contains('todo-complete')) this.completedItem(target.closest('.todo-item').key);
-        if(target.classList.contains('todo-edit')) this.editItem(target, target.closest('.todo-item').key);
-    };
-    deleteItem(selectedItemKey){
-        for(let itemKey of this.todoData.keys()){
-            if(itemKey === selectedItemKey) this.todoData.delete(itemKey);
-        }
-        this.render();
-    };
-    completedItem(selectedItemKey){
-        for(let itemKey of this.todoData.keys()){
-            if(itemKey === selectedItemKey){
-                this.todoData.get(itemKey).completed = this.todoData.get(itemKey).completed ? false : true;
-            }
-        }
-        this.render();
-    };
-    editItem(editBtn, selectedItemKey){
-        let selectedSpan = editBtn.closest('.todo-item').firstElementChild;
-        selectedSpan.contentEditable = true;
-        selectedSpan.focus();
-        console.log(selectedSpan.textContent);
-        this.todoContainer.addEventListener('click', (evt) => {
-            if(evt.target !== selectedSpan){
-                selectedSpan.contentEditable = false;
-                for(let itemKey of this.todoData.keys()){
-                    if(itemKey === selectedItemKey) 
-                    this.todoData.get(itemKey).value = selectedSpan.textContent;
+        document.addEventListener('keydown', (evt) => {
+            if(evt.code === 'Enter' && this.isEditTodo) {
+                evt.preventDefault();
+                this.saveEdit();
                 }
+        });
+        this.render();
+    };
+    handler(evt){
+        let target = evt.target;
+        if(this.isEditTodo && target){
+            this.saveEdit();
+            return;
+        }
+        if(target.classList.contains('todo-remove')) this.deleteItem(target);
+        if(target.classList.contains('todo-complete')) this.completedItem(target);
+        if(target.classList.contains('todo-edit')) {
+            this.isEditTodo = true;
+            this.editItem(target);
+        }
+    };
+    deleteItem(deleteBtn){
+        const selectedLi = deleteBtn.closest('.todo-item');
+        const selectedLiKey = selectedLi.key;
+        this.animate ({
+            duration: 2000,
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                selectedLi.style.opacity = 1 - progress;
             }
-            this.render();
         });
         
+        this.todoData.delete(selectedLiKey);
+        setTimeout(this.render, 2100);
     };
-    addTodo(e){
-        e.preventDefault();
+    completedItem(selectedLiKey){
+        const selectedLi = deleteBtn.closest('.todo-item');
+        const selectedLiKey = selectedLi.key;
+        this.todoData.get(selectedLiKey).completed = this.todoData.get(selectedLiKey).completed ? false : true;
+        if(this.todoData.get(selectedLiKey).completed){
+            this.animate ({
+                duration: 2000,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    selectedLi.style.opacity = 1 - progress;
+                }
+            });
+        }
+        this.render();
+    };
+    editItem(editBtn){ 
+        const selectedSpan = editBtn.closest('.todo-item').firstElementChild;
+        selectedSpan.contentEditable = true;
+        selectedSpan.focus();
+    };
+    saveEdit(){
+        const todoItemEdit = document.querySelectorAll('.text-todo');
+        todoItemEdit.forEach((selectedSpan) => {
+            if(selectedSpan.isContentEditable){
+                const selectedLiKey = selectedSpan.closest('.todo-item').key;
+                selectedSpan.contentEditable = false;
+                this.todoData.get(selectedLiKey).value = selectedSpan.textContent;
+                this.render();
+            }
+        });
+    };
+    addTodo(evt){
+        evt.preventDefault();
         if(this.input.value.trim()){
             const newTodo = {
                 value: this.input.value,
@@ -70,6 +102,7 @@ class ToDo {
     render (){
         this.todoList.textContent = '';
         this.todoCompleted.textContent = '';
+        this.isEditTodo = false;
         this.todoData.forEach(this.createItem, this);
         this.addToStorage();
     };
@@ -78,7 +111,7 @@ class ToDo {
         li.classList.add('todo-item');
         li.key = todoItem.key;
         li.insertAdjacentHTML('beforeend', `
-            <span class="text-todo" contenteditable="false">${todoItem.value}</span>
+            <span class="text-todo">${todoItem.value}</span>
             <div class="todo-buttons">
                 <button class="todo-edit"></button>
                 <button class="todo-remove"></button>
@@ -89,6 +122,18 @@ class ToDo {
     };
     addToStorage() {
         localStorage.setItem('ToDo', JSON.stringify([...this.todoData]));
+    };
+    animate ({timing, draw, duration}, callback) {
+        let start = performance.now();
+        requestAnimationFrame(function animate(time) {
+            let timeFraction = (time - start) / duration;
+            if (timeFraction > 1) timeFraction = 1;
+            let progress = timing(timeFraction);
+            draw(progress); 
+            if (timeFraction < 1) {
+                requestAnimationFrame(animate);
+            }
+        });
     };
 };
 
