970808d42e88ba0943f18b57faa4814df897c8d6
Kirill Sukharev <aruter@yandex.ru>
2020-11-16 21:46:26 +0300

    lesson28 is done

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,34 @@
-let money = 1000000, 
-    income = '2000000', 
-    addExpenses = 'food, internet, goods', 
-    deposit = true, 
-    mission = 5000000, 
-    period = 9,
-    budgetDay = money/30,
-    num = 266219,
-    numArr = num.toString().split(''),
-    numResult = 1;
+const output = document.getElementById('output');
+const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
 
-numArr.forEach(function(item){
-    numResult *= item;
-});
-let exNumResult = numResult**3;
-let numResultArr = exNumResult.toString().split('');
+const getData = (url, body) => {
+    return new Promise ((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('POST', url);
+        request.setRequestHeader('Content-Type', 'application/json')
+        request.addEventListener('readystatechange', () => {
+        if(request.readyState !== 4){
+            return;
+        }
+        if(request.status === 201){
+            const response = JSON.parse(request.responseText);
+            resolve(response);
+        }else {
+            reject(request.statusText);
+        }
+    });
+        request.send(JSON.stringify(body));
+    });
+};
 
-//Low block
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей");
-console.log(addExpenses.toLocaleLowerCase().split(''));
-console.log(budgetDay);
-// Hard block
-console.log(numResult);
-console.log(exNumResult);
-console.log(numResultArr[0], numResultArr[1]);
+const body = {
+    "name": "Kirill",
+    "albumId": 30,
+    "title": "aaskskkkk dkdkkdkfkf !!!!k dkdkdkkd",
+    "url": "https://study.up-skills.ru/",
+    "thumbnailUrl": "https://study.up-skills.ru/"
+}
 
-
-
-
-
+getData(urlPhotos, body)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
