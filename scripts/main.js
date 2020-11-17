0fb862ef84d7fe4531435fcf772357494baa79e5
Kirill Sukharev <aruter@yandex.ru>
2020-11-17 19:14:46 +0300

    added smooth appearance of timer digits

--- scripts/main.js
+++ scripts/main.js
@@ -6,37 +6,36 @@ window.addEventListener("DOMContentLoaded", () => {
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
     
-    const getTimeRemaning = () => {
-        let dateStop = new Date(deadline).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
-            seconds = Math.floor(timeRemaining % 60),
-            minutes = Math.floor((timeRemaining / 60) % 60),
-            hours = Math.floor((timeRemaining / 60 / 60)/24);
-        return {timeRemaining, hours, minutes, seconds};
-    };
+        const getTimeRemaning = () => {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor((timeRemaining / 60 / 60)/24);
+            return {timeRemaining, hours, minutes, seconds};
+        };
 
-    const formatTime = (data) => {
-        if (data < 10) return '0' + data;
-        else return data;
-    };
+        const formatTime = (data) => {
+            if (data < 10) return '0' + data;
+            else return data;
+        };
 
-    const timerId = setInterval( () => {    
-        let timer = getTimeRemaning();
-        timerHours.textContent = formatTime(timer.hours);
-        timerMinutes.textContent = formatTime(timer.minutes);
-        timerSeconds.textContent = formatTime(timer.seconds);
-            
+        const timerUpdate = () => {
+            let timer = getTimeRemaning();
+            timerHours.textContent = formatTime(timer.hours);
+            timerMinutes.textContent = formatTime(timer.minutes);
+            timerSeconds.textContent = formatTime(timer.seconds);
             if (timer.timeRemaining < 0) {
-                clearInterval(timerId);
-                timerHours.style.color = 'red';
-                timerMinutes.style.color = 'red';
-                timerSeconds.style.color = 'red';
+                clearInterval(call);
+                document.getElementById('timer').style.color = 'red';
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
             }
-        }, 1000);
+        };
+        timerUpdate();
+        const call = setInterval(timerUpdate, 1000);
     };
     countTimer('31 december 2020');
     // Header menu
